cd08987c4330f3ce4fbbc3d7a1409a901a268dc0
Takuto <pysan3@gmail.com>
2019-07-31 10:03:10 +0900

    add frontend

---
+++ frontend/vue.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    outputDir: '../static/',
+    assetsDir: 'dist',
+    publicPath: './static/'
+}
--- run_main.py
+++ run_main.py
@@ -1,8 +1,11 @@
 import responder
 
-api = responder.API()
+api = responder.API(templates_dir='static')
 api.add_route('/',  static=True, websocket=True)
 
 @api.route('/api/login')
 async def login(req, resp):
     pass
+
+if __name__ == '__main__':
+    api.run()
